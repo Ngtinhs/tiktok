a7d6c904065d546d177b4ec1e172ecebeee39ad4
Ngtinh <19dh110978@st.huflit.edu.vn>
2022-07-13 13:28:33 +0700

    Đưa cấu hình route ra ngoài

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Ngtinhs</h1>
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
